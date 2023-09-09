5b1caf08db1fc94481743452bffd7e405ac3bf19
AlexeyOrlovsky <orlovskyiosdev@gmail.com>
2023-09-09 23:00:40 +0300

    Initial commit

---
+++ TasksApp/View/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TasksApp
+//
+//  Created by Алексей Орловский on 09.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
